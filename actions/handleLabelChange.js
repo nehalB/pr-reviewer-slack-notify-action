3e44420e5b3a9b84af5f28bf4d09cdaf66837cc2
Nehal Bhagat <nehal.bhagat@appdirect.com>
2020-12-14 22:49:18 -0500

    added label with slack group

--- actions/handleLabelChange.js
+++ actions/handleLabelChange.js
@@ -6,19 +6,30 @@ const { slackWebClient, getSlackMessageId } = require("../utils");
 module.exports = async () => {
   try {
     const channelId = core.getInput("channel-id");
-    const labelNameToWatchFor = core.getInput("label-name-to-watch-for");
+    const labelNameToWatchFor = JSON.parse(core.getInput("labels-for-slack-groups"));
     const slackUsers = JSON.parse(core.getInput("slack-users"));
     const { pull_request, repository, sender } = github.context.payload;
 
     // if there is now a matching label added, notify the slack message
-    let hasLabel = false;
-    pull_request.labels.forEach((label) => {
-      if (label.name === labelNameToWatchFor) {
-        hasLabel = true;
-      }
+    // let hasLabel = false;
+    // pull_request.labels.forEach((label) => {
+    //   if (label.name === labelNameToWatchFor) {
+    //     hasLabel = true;
+    //   }
+    // });
+
+    // atm only one pair of label_name/slack_group is expected to be added in PR since we can't keep track of which one is old and which one is new, otherwise all the labels will be notified every time.
+
+    let groups = [];
+    pull_request.labels.forEach((prLabel) => {
+      labelNameToWatchFor.forEach((label) => {
+        if (prLabel.name === label.label_name) {
+          groups.push(label);
+        }
+      })
     });
 
-    if (!hasLabel) {
+    if (groups.length === 0) {
       return null;
     }
 
@@ -29,20 +40,25 @@ module.exports = async () => {
       return user.github_username === pull_request.user.login;
     });
 
-    const plainText = `<@${author.slack_id}>, ${labeler.github_username} added the label ${labelNameToWatchFor} to your PR`;
-    const richText = `<@${author.slack_id}>, *${labeler.github_username}* added the label *${labelNameToWatchFor}* to your PR`;
+    let notificationMessage;
+    labelGroup.forEach((label) => {
+      notificationMessage = notificationMessage + `<@${label.slack_group}>, ${labeler.github_username} added the label ${labelNameToWatchFor} to the PR\n`;
+    });
+
+    // const plainText = `<@${author.slack_id}>, ${labeler.github_username} added the label ${labelNameToWatchFor} to your PR`;
+    // const richText = `<@${author.slack_id}>, *${labeler.github_username}* added the label *${labelNameToWatchFor}* to your PR`;
     const slackMessageId = await getSlackMessageId(pull_request, repository);
 
     await slackWebClient.chat.postMessage({
       channel: channelId,
       thread_ts: slackMessageId,
-      text: plainText,
+      text: notificationMessage,
       blocks: [
         {
           type: "section",
           text: {
             type: "mrkdwn",
-            text: richText,
+            text: notificationMessage,
           },
         },
       ],
